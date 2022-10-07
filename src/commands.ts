009007ee501a8c1dd0c10c353e195f60d9e4b7ca
zomixi <zomixi@qq.com>
2022-10-07 19:45:00 +0800

    feat: add snake-case

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,7 @@
 import { camelCase } from "camel-case";
 import { constantCase } from "constant-case";
 import { pascalCase } from "pascal-case";
+import { snakeCase } from "snake-case";
 import { commandBuilder } from "./commandBuilder";
 
 const COMMANDS = [
@@ -15,6 +16,10 @@ const COMMANDS = [
   {
     id: "naming-convention.constant-case",
     callback: commandBuilder(constantCase),
+  },
+  {
+    id: "naming-convention.snake-case",
+    callback: commandBuilder(snakeCase),
   },
 ];
 
